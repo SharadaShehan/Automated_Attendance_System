e6f2536344f3157295823264edfc242f07549b2d
Sharada Shehan <sharadashehan6@gmail.com>
2023-09-15 18:46:50 +0530

    base folder values set

--- backend/backend/asgi.py
+++ backend/backend/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for attendsense_prototype project.
+ASGI config for Automated_Attendance_System project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'attendsense_prototype.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
 
 application = get_asgi_application()
--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for attendsense_prototype project.
+URL configuration for Automated_Attendance_System project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
