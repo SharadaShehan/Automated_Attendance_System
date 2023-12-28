dbe2ab3aa3da5892fa4f83a241590cf13ff01d14
Sharada Shehan <sharadashehan6@gmail.com>
2023-12-29 00:03:21 +0530

    bin folder conflict fixed

--- cam_app/main.py
+++ cam_app/main.py
@@ -1,6 +1,10 @@
 from components.MainComponent import UIApp
+import os
 
 
 if __name__ == "__main__":
+    bin_folder = 'bin'
+    if not os.path.exists(bin_folder):
+        os.makedirs(bin_folder)
     app = UIApp()
     app.mainloop()
