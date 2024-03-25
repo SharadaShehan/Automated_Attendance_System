1f33c374e80420f02d8e1dd4ea169b6d12cb2377
Sharada Shehan <sharadashehan6@gmail.com>
2024-03-25 09:13:06 +0530

    run mosquitto broker script

--- run_mosquitto_broker.sh
+++ run_mosquitto_broker.sh
@@ -10,22 +10,36 @@ fi
 user_name="$1"
 password="$2"
 
+# Check if all arguments are provided
 if [ -z "$user_name" ] || [ -z "$password" ]; then
   echo "Please provide all arguments: user_name, password"
   exit 1
 fi
 
+# Install mosquitto broker
 sudo apt update
 sudo apt install mosquitto mosquitto-clients -y
-sudo mosquitto_passwd -c /etc/mosquitto/passwd "$user_name" "$password"
+
+# create a password file and add user
+password_file="/etc/mosquitto/passwd"
+sudo cat << EOF > "$password_file"
+$user_name:$password
+EOF
+
+# encrypt the password file
+sudo mosquitto_passwd -U "$password_file"
 
 config_file="/etc/mosquitto/conf.d/default.conf"
 sudo cat << EOF > "$config_file"
 allow_anonymous false
-password_file /etc/mosquitto/passwd
+password_file file_name
 listener 1883
 EOF
 
+# replace file_name with the password file
+sudo sed -i "s/file_name/$password_file/g" "$config_file"
+
+# restart mosquitto broker
 sudo systemctl restart mosquitto
 
 # print success message
