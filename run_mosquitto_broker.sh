f83e7e8972d76805b54121c915edf4e462317ed0
Sharada Shehan <sharadashehan6@gmail.com>
2024-03-25 09:19:29 +0530

    minor bug fix

--- run_mosquitto_broker.sh
+++ run_mosquitto_broker.sh
@@ -21,23 +21,19 @@ sudo apt update
 sudo apt install mosquitto mosquitto-clients -y
 
 # create a password file and add user
-password_file="/etc/mosquitto/passwd"
-sudo cat << EOF > "$password_file"
+sudo cat << EOF > "/etc/mosquitto/passwd"
 $user_name:$password
 EOF
 
 # encrypt the password file
-sudo mosquitto_passwd -U "$password_file"
+sudo mosquitto_passwd -U "/etc/mosquitto/passwd"
 
 config_file="/etc/mosquitto/conf.d/default.conf"
 sudo cat << EOF > "$config_file"
 allow_anonymous false
-password_file file_name
+password_file /etc/mosquitto/passwd
 listener 1883
 EOF
-
-# replace file_name with the password file
-sudo sed -i "s/file_name/$password_file/g" "$config_file"
 
 # restart mosquitto broker
 sudo systemctl restart mosquitto
